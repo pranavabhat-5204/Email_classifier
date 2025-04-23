e1e427edaf2c47f0a521db9c6ae7379339a4bb85
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-04-23 22:54:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Importing the necessary libraries
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
@@ -10,16 +11,23 @@ import numpy as np
 from flask import Flask, render_template, request
 import spacy
 from sklearn.preprocessing import LabelEncoder
+
+#Loading nl_core_news_sm using spacy for NER masking 
 nlp = spacy.load("nl_core_news_sm")
 import torch
 import torch.nn as nn
+
 app=Flask(__name__, template_folder=r"C:Users/SDP/Desktop")
+
+#Now we need to create a few variables and also provide the vocabulary for decoding the output of the model
 data=pd.read_csv("E:/combined_emails_with_natural_pii.csv")
 X_train, X_test, y_train, y_test = train_test_split(data['email'], data['type'], test_size=0.2, random_state=42)
 label_encoder = LabelEncoder()
 vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 X_train = vectorizer.fit_transform(X_train)
 y_train = label_encoder.fit_transform(y_train)
+
+#Loading the model
 model_path = 'E:/your_model_name1.pth'
 model = nn.Sequential(
     nn.Linear(28113, 64),   # First layer: fully connected
@@ -36,6 +44,8 @@ def email():
 def result():
   if request.method=='POST':
     result=request.form['Data']
+
+    #Masking the Text
     doc=nlp(str(result))
     masked_text=str(result)
     for ent in doc.ents:
@@ -46,7 +56,7 @@ def result():
 
     # Vectorize the input text before prediction
     result_vectorized = vectorizer.transform([str(result)])
-
+    
     result_prediction = model(torch.tensor(result_vectorized.toarray(), dtype=torch.float32)) 
     predicted_class_index = torch.argmax(result_prediction, dim=1).item()
     class_names = label_encoder.classes_ # Access the correct attribute 'classes_'
